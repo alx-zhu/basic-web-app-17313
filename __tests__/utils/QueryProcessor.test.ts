d362b2176d62f6f17e088b5d9a4fc2d398103694
Alex Zhu <alexanderzhu07@gmail.com>
2024-10-07 11:18:40 -0400

    Added query for andrew id

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,20 +1,26 @@
 import QueryProcessor from "../../utils/QueryProcessor";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 describe("QueryProcessor", () => {
-    test("should return a string", () => {
-        const query = "test";
-        const response: string = QueryProcessor(query);
-        expect(typeof response).toBe("string");
-    });
+  test("should return a string", () => {
+    const query = "test";
+    const response: string = QueryProcessor(query);
+    expect(typeof response).toBe("string");
+  });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
+  test("should return shakespeare description", () => {
+    const query = "shakespeare";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe(
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+        "English poet, playwright, and actor, widely regarded as the greatest " +
+        "writer in the English language and the world's pre-eminent dramatist."
+    );
+  });
+
+  test("should return andrew id", () => {
+    const query = "what is your andrewid";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("azhu3");
+  });
 });
